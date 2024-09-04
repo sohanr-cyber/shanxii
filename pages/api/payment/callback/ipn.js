6b01c24e6b8a78a8bf8102ef4eb6b361d93517f6
sohanr-cyber <81401762+sohanr-cyber@users.noreply.github.com>
2024-09-04 12:42:37 +0600

    0285320

--- pages/api/payment/callback/ipn.js
+++ pages/api/payment/callback/ipn.js
@@ -13,10 +13,12 @@ const store_passwd = process.env.store_passwd
 const is_live = process.env.is_live == 'false' ? false : true //true for live, false for sandbox
 
 handler.post(async (req, res) => {
-  console.log('incomming')
+  console.log('----incomming')
   const { tran_id, val_id, amount, currency, status } = req.body
+  console.log({ tran_id, val_id, amount, currency, status })
   try {
     const order = await Order.findOne({ trackingNumber: tran_id })
+    console.log({ order })
     if (status === 'VALID' && order.total === amount) {
       order.paymentStatus = 'completed'
       await order.save()
