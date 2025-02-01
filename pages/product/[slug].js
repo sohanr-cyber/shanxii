9e41c2de9f44c24ecf2756f475eb8d15036aac7f
sohanr-cyber <sohanur25800@gmail.com>
2025-02-01 12:41:11 +0600

    0280325

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -76,8 +76,7 @@ export async function getStaticProps(context) {
       relatedProducts = resp.data.products.filter(i => i._id != data._id)
     }
 
-    let reviews = []
-    // const { data: reviews } = await axios.get(`${BASE_URL}/api/review/${data._id}`)
+    const { data: reviews } = await axios.get(`${BASE_URL}/api/review/${data._id}`)
 
     console.log(`Data fetching time: ${end - start}ms`)
 
@@ -370,9 +369,9 @@ const Product = ({ product, error, relatedProducts, reviews }) => {
             <button className={styles.button} onClick={() => setOpen("description")}>Description</button>
             <button className={styles.button} onClick={() => setOpen("reviews")}>Reviews</button>
           </div>
-          {/* {open == "description" ? <div className={styles.description}>
+          {open == "description" ? <div className={styles.description}>
             <div dangerouslySetInnerHTML={{ __html: product.description }} />
-          </div> : isClient && <ReviewList product={product._id} reviews={reviewList} />} */}
+          </div> : isClient && <ReviewList product={product._id} reviews={reviewList} />}
         </div>
       </div>
       {relatedProducts?.length > 0 && (
